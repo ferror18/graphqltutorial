61d0b8a648a49947ff9218e3ecb8b86bbd5a8286
ferror18 <ferror18@gmail.com>
2021-04-27 15:56:17 -0700

    .

--- start/server/index.js
+++ start/server/index.js
@@ -1,1 +1,23 @@
-console.log('Hello')
+const { ApolloServer, gql } = require('apollo-server')
+
+const typeDefs = gql`
+    type Book {
+        title: String
+        author: String
+    }
+
+    type Query {
+    books: [Book]
+    }
+`;
+
+const books = [
+    {
+        title: 'lorem',
+        author: 'ipsum'
+    },
+    {
+        title: 'ipsum',
+        author: 'lorem'
+    }
+]
